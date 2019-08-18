76ae7bdc50e55cb165e4e74dd87a0690dc597390
unknown <ishii12y3@gmail.com>
2019-08-18 13:53:18 +0900

    Add my excellent code

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 // 食物連鎖のシミュレータ
 const INIT = 16; // 生物の初期サイズ
 const MIN = 12; // 生存最小サイズ
-const MAX = 128; // 成熟サイズ
-const NEAR = 100; // 隣接判定距離
-const DENSITY = 8; // 共存可能個体数
-const RANGE = 300; // 種子が飛散する最大距離
+const MAX = 50; // 成熟サイズ
+const NEAR = 16; // 隣接判定距離
+const DENSITY = 16; // 共存可能個体数
+const RANGE = 80; // 種子が飛散する最大距離
 
 let plants = new Plants();
 
@@ -20,6 +20,6 @@ function draw() {
   plants.update();
   plants.draw();
   textSize(32);
-  fill(255);
+  fill(122);
   text(plants.size, 10, 50);
 }
